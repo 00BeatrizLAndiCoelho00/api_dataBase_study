7c05fd96e8e93007484cc49869a4eb576c9e7338
ANDRESSA BEATRIZ FIDELIS LANDI COELHO <22282076@CFP127ALUNO.LOCAL>
2023-04-14 11:26:29 -0300

    a

---
+++ app.js
@@ -0,0 +1,93 @@
+/**________________________________________________________________________________________________________________________________________________________________________//
+ * 
+ * Objective : Create an api tahth will alow aacess a database(GET POST DELET PUT)
+ * Author: A. Beatriz Fidelis Landi Coelho
+ * Data : 14-04-2023
+ * Version: 1.0 
+ * _____________________________________________________________________________________________________________________________________________________________//
+ * 
+ *  Express - permite a integraçao entre http com o codigo 
+ * npm install express --save
+ * 
+ * Cors - Tambem usa-se o cors (um gerenciador de permissoes para o protocolo htttp 
+ * npm instal cors --save)
+ * 
+ * Body-paser  - è uma dependencia que permite manipular dados enviados pela requisicap
+ * npm install body-parser --save
+ *  _________________________________________________________________________________________________________________________________________________________________*/
+
+
+//______________________________________________________INSTALACAO PRISMA (QUE ME PERMITE ACESSAR O BANCO DE DADOS)_______________________________________________________//
+
+
+    //npm install prisma --save
+    //npx prisma
+    //npm prisma init
+    //npm install @prisma/cliente
+
+
+//__________________________________________IMPORT DAS DEPENDENCIAS_______________________________________________________________________________________________________//
+
+const express = require('express')
+const cors = require('cors')
+const bodyParse = require('body-parse')
+const { request } = require('http')
+const { response } = require('express')
+
+//____________________________________________CRIA O IBJETO_______________________________________________________________________________________________________
+
+const app = express()
+
+//configura as permissoes do cors
+
+app.use((request,response, next)=>{
+    response.header('Acess-Control-Allow_Origin','*')
+    response.header('Acess-Control-Allow_Methods','GET, POST, PUT, DELETE,OPTIONS')
+
+    app.use(cors())
+
+    next()
+})
+
+//______________________________________________________ENDPOINTS BANCO DE DADOS________________________________________________________________________________________________// 
+
+/*CRUD 
+      CREATE
+      READ
+      UPDATE
+      DELETE
+*/
+
+
+
+    //retorna todos os dados de alunos
+    app.get('/v1/lion-school/alunos',cors(),async function(request,response){
+
+
+    })
+
+    //retorna dados do aluno pelo id
+    app.get('/v1/lion-school/aluno/:id',cors(),async function(request,response){
+
+
+    })
+
+    //Insere um novo aluno
+    app.post('/v1/lion-school/aluno/',cors(),async function(request,response){
+
+
+    })
+
+    //atualiza um aluno pelo id
+    app.put('/v1/lion-school/aluno/:id',cors(),async function(request,response){
+
+
+    })
+
+
+    //exclui um aluno pelo id
+    app.delete('/v1/lion-school/aluno/:id',cors(),async function(request,response){
+
+
+    })
+//_________________________________________________________________________________________________________________________________________________________________//
---
+++ model/DAO/aluno.DAO.js
@@ -0,0 +1,30 @@
+/**________________________________________________________________________________________________________________________________________________________________________//
+ * 
+ * Objective : Create an api tahth will alow aacess a database(GET POST DELET PUT)
+ * Author: A. Beatriz Fidelis Landi Coelho
+ * Data : 14-04-2023
+ * Version: 1.0 
+ * _____________________________________________________________________________________________________________________________________________________________*/
+
+
+//insere novo registro no banco de dados 
+const insertAluno = function(dadosAluno){
+
+}
+//atualiza dados
+const updateAluno = function(dadosAluno){
+
+}
+//exclui dados
+const deleteAluno = function(id){
+
+}
+//aretorna alunos
+const selectAllAluno = function(){
+
+}
+
+const selectByIdAluno = function(id){
+
+}
+
